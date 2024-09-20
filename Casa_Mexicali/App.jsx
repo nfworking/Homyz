beea4d786eacfc3f8ccb378ded253612615f2751
Cal <168704009+nfworking@users.noreply.github.com>
2024-09-20 14:57:32 +1000

    tried to put product details

--- Casa_Mexicali/App.jsx
+++ Casa_Mexicali/App.jsx
@@ -1,7 +1,7 @@
 import HeroSection from "../src/components/1/HeroSection"
 import Navbar from "../src/components/1/Navbar"
-import HouseDetails from "../src/components/1/House-details"
-import Footer from "../src/components/1/Footer"
+import Carousel from "../src/components/1/HousePage"
+
 
 
 const App = () => {
@@ -9,8 +9,7 @@ const App = () => {
     <div className="overflow-y-hidden text-neutral-200">
       <HeroSection/>
       <Navbar />
-      <HouseDetails/>
-     
+      <Carousel/>
     </div>
   )
 }
