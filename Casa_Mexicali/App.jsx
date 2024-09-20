1ebba3a996d38a58c7cc52cc0497b82918cb0d3c
Cal <168704009+nfworking@users.noreply.github.com>
2024-09-20 15:24:41 +1000

    idk

--- Casa_Mexicali/App.jsx
+++ Casa_Mexicali/App.jsx
@@ -1,6 +1,6 @@
 import HeroSection from "../src/components/1/HeroSection"
 import Navbar from "../src/components/1/Navbar"
-import Carousel from "../src/components/1/HousePage"
+
 
 
 
@@ -9,7 +9,7 @@ const App = () => {
     <div className="overflow-y-hidden text-neutral-200">
       <HeroSection/>
       <Navbar />
-      <Carousel/>
+
     </div>
   )
 }
