cee3cc41969b362119c909d4e786857fc44e7ad4
Cal <168704009+nfworking@users.noreply.github.com>
2024-09-21 09:43:15 +1000

    edited stripe payment

--- Coobowie_Waters/App.jsx
+++ Coobowie_Waters/App.jsx
@@ -1,12 +1,19 @@
-import Checkout from "../src/components/1/PaymentPage"
+import ProductPage from "../src/components/1/HousePage"
+import HeroSection from "../src/components/2/HeroSection"
+import Footer from "../src/components/1/Footer"
+import ContactSection from "../src/components/ContactSection"
 
 
 const App = () => {
   return (
     <div className="overflow-y-hidden text-neutral-200">
-      <Checkout/>
+
+    <ProductPage/>
+    <ContactSection/>
+    <Footer/>
     </div>
   )
+
 }
 
 export default App
