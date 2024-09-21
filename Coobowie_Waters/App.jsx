f148f6980f4cd01ef59cae607a988bc313035f85
Cal <168704009+nfworking@users.noreply.github.com>
2024-09-21 11:08:26 +1000

    property 2 done

--- Coobowie_Waters/App.jsx
+++ Coobowie_Waters/App.jsx
@@ -2,12 +2,14 @@ import ProductPage from "../src/components/2/HousePage"
 import HeroSection from "../src/components/2/HeroSection"
 import Footer from "../src/components/2/Footer"
 import ContactSection from "../src/components/ContactSection"
+import Navbar from "../src/components/2/Navbar"
 
 
 const App = () => {
   return (
     <div className="overflow-y-hidden text-neutral-200">
       <HeroSection/>
+      <Navbar/>
     <ProductPage/>
     <ContactSection/>
     <Footer/>
